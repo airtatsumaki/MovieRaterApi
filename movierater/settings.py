e4189c7b311d6b05804b22b92ed5cc87bf2561e2
tatsu <naz1mb20@gmail.com>
2021-12-10 11:23:27 +0000

    update ALLOWED_HOSTS for heroku

--- movierater/settings.py
+++ movierater/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['.ngrok.io','localhost','127.0.0.1']
+#ALLOWED_HOSTS = ['.ngrok.io','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['movie-rater-naz.herokuapp.com']
 
 
 # Application definition
